cdf5cf92710193de2cd5247fb71df192e090517c
WisdomWolf <wisdomwolf@gmail.com>
2022-01-21 13:53:43 -0500

    Implements working pipeline for conversion

--- server.js
+++ server.js
@@ -9,27 +9,38 @@ app.get('/', (req, res) => {
   res.send('Successful response.');
 });
 
-async function download() {
-    
-}
+app.get('/downloader', async(req, res) => {
+    const downloader = new Downloader({
+        url: "https://raw.githubusercontent.com/WisdomWolf/curriculum_vitae/gh-pages/README.md", //If the file name already exists, a new file with the name 200MB1.zip is created.
+        directory: "./", //This folder will be created, if it doesn't exist.
+        cloneFiles: false, //This will cause the downloader to re-write an existing file.
+    });
+    try {
+        downloader.download(); //Downloader.download() returns a promise.
+        console.log('Markdown download complete')
+    } catch (error) {
+        //IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
+        //Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
+        console.log("Download failed", error);
+    }
+    res.send('Download success')
+});
 
 
 app.get('/download', async(req, res) => {
     const downloader = new Downloader({
         url: "https://raw.githubusercontent.com/WisdomWolf/curriculum_vitae/gh-pages/README.md", //If the file name already exists, a new file with the name 200MB1.zip is created.
-        directory: "./downloads", //This folder will be created, if it doesn't exist.
+        directory: ".", //This folder will be created, if it doesn't exist.
         cloneFiles: false, //This will cause the downloader to re-write an existing file.
     });
     try {
         await downloader.download(); //Downloader.download() returns a promise.
         console.log('Markdown download complete')
-        fs.createReadStream("./downloads/README.md")
-            .pipe(markdownpdf())
-            .pipe(fs.createWriteStream("./resume.pdf"));
-        console.log('PDF Conversion Complete')
-        const file = `${__dirname}/resume.pdf`;
-        res.download(file);
-        console.log('File download should be happening')
+        console.log('starting markdown conversion')
+        markdownpdf().from("./README.md").to("./resume.pdf", function () {
+            const file = `${__dirname}/resume.pdf`;
+            res.download(file);
+        });
     } catch (error) {
         //IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
         //Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
@@ -38,11 +49,13 @@ app.get('/download', async(req, res) => {
     // res.send('File downloaded successfully')
 });
 
-app.get('/convert', function(req, res) {
-    fs.createReadStream("./downloads/README.md")
-        .pipe(markdownpdf())
-        .pipe(fs.createWriteStream("./resume.pdf"));
-    res.send('Converion complete')
+app.get('/convert', async(req, res) => {
+    console.log('starting markdown conversion')
+    markdownpdf().from("./README.md").to("./resume.pdf", function () {
+        const file = `${__dirname}/resume.pdf`;
+        res.download(file);
+    });
+    console.log('conversion complete')
  });
 
  app.get('/direct-download', function(req, res) {
