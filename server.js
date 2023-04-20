f231dc45b96934ad99eb026b6198614af2b0979e
WisdomWolf <wisdomwolf@gmail.com>
2023-04-20 14:39:10 -0400

    Uses tmp directory to store downloads

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const fs = require("fs");
 const app = express();
 
 const source_url = "https://raw.githubusercontent.com/WisdomWolf/curriculum_vitae/gh-pages/README.md"
-const pdf_file = `${__dirname}/resume.pdf`;
-const md_file = `${__dirname}/README.md`;
+const pdf_file = `/tmp/resume.pdf`;
+const md_file = `/tmp/README.md`;
 
 app.get('/', (req, res) => {
     res.send('Success!')
@@ -24,7 +24,7 @@ app.get('/download', async(req, res) => {
     console.log('url: ' + url);
     const downloader = new Downloader({
         url: url,
-        directory: `${__dirname}`,
+        directory: `/tmp`,
         cloneFiles: false, //This will cause the downloader to re-write an existing file.
     });
     try {
