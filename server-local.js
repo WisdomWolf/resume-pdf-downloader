e4228bccef53ea14e490c54b85756f410c972c66
WisdomWolf <wisdomwolf@gmail.com>
2023-04-20 14:25:34 -0400

    Removes Netlify files

--- server-local.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const app = require('./express/server');
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port, () => console.log('App is listening on port: ' + port));
